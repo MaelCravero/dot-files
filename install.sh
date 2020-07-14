83e106e3bf36d4b41059ed332dd622f07ed0dcad
Mael Cravero <mael.cravero@epita.fr>
2020-07-14 18:22:28 +0200

    Add rofi config

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ ln -sT $PWD/i3 ~/.i3 && echo "i3 ok"
 
 ln -s $PWD/vimrc ~/.vimrc && echo "vimrc ok"
 ln -sT $PWD/config/nvim ~/.config/nvim && echo "config/nvim ok"
+ln -sT $PWD/config/rofi ~/.config/rofi && echo "config/rofi ok"
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim && \
