804cf2dd42451dc25a818ec6f4616ef2f8497677
Mael Cravero <mael.cravero@epita.fr>
2020-10-05 22:44:57 +0200

    bspwm: update config

--- bspwm/startup.sh
+++ bspwm/startup.sh
@@ -3,10 +3,14 @@
 # Startup programs
 setxkbmap -option "caps:escape"
 redshift-gtk -g 0.9:0.9:0.7 &
+nm-applet &
 
-pulseaudio
+pulseaudio &
 picom -b
 
-discord --start-minimized
-slack -u
-thunderbird
+discord --start-minimized &
+slack -u &
+thunderbird &
+
+# Alt-Tab switcher
+# alttab -bg '#282828' -fg '#fbf1c7' -frame '#d79921' -i '64x64' &
